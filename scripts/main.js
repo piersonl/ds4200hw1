3e8f58419a1da4498b9a6fbbe649222898c845fc
lon pierson <you@example.com>
2024-01-23 18:15:28 -0500

    initialize

--- scripts/main.js
+++ scripts/main.js
@@ -10,22 +10,26 @@ myImage.onclick = () => {
 };
 
 let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myHeading = document.querySelector("h2");
 
 myButton.onclick = () => {
     setUserName();
   };
 
-function setUserName() {
+  function setUserName() {
     const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Hello, ${myName}`;
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Have a good day, ${myName}!`;
+    }
   }
 
   if (!localStorage.getItem("name")) {
     setUserName();
   } else {
     const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Hello, ${storedName}`;
+    myHeading.textContent = `Have a good day, ${storedName}!`;
   }
 
